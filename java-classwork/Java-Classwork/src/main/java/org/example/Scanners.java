c11c892ff1abc30d6d39c2c139605f99d9366641
LAPTOP-07M3D52O\codenizam <codenizam@gmail.com>
2024-05-02 15:36:22 -0400

    2nd set of GLABs

--- java-classwork/Java-Classwork/src/main/java/org/example/Scanners.java
+++ java-classwork/Java-Classwork/src/main/java/org/example/Scanners.java
@@ -6,6 +6,6 @@ public class Scanners {
     public static void main (String[] args ){
         Scanner sc1 = new Scanner(System.in);
         System.out.println("Hello = ");
-
+int sum = sc1.nextInt();
     }
 }
