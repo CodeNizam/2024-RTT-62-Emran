87b8bde8a8d62ff58e73478fd8350a0081a85bf8
LAPTOP-07M3D52O\codenizam <codenizam@gmail.com>
2024-05-02 00:40:29 -0400

    Homework - PA 303.2.1 - Practice Assignment Core Java - Variables

---
+++ java-classwork/Java-Classwork/src/main/java/org/example/CoreJavaBasics.java
@@ -0,0 +1,72 @@
+package org.example;
+
+public class CoreJavaBasics {
+    public static void main(String args[]) {
+
+        //Question #1
+        int num1 = 1;
+        int num2 = 2;
+        int sum = num1 + num2;
+        System.out.println(sum);
+
+        //Question #2
+        double dub1 = 12.5;
+        double dub2 = 17.5;
+        double sumDub = dub1+dub2;
+        System.out.println(sumDub);
+
+        //Question #3
+        int num3 = 3;
+        double dub3 = 2.5;
+        double sumIntDub= num3+ dub3;
+        System.out.println(sumIntDub);
+
+        //Question #4
+        int num4 = 5;
+        int num5 = 10;
+        int divide = num5 / num4;
+        System.out.println(divide);
+
+        double num6 = 12.5;
+        double divideDecimal = num6/num4;
+        System.out.println(divideDecimal);
+
+        //Question #5
+        double dub4 = 12.5;
+        double dub5 = 26.00;
+        double sumDub2 = dub5/dub4;
+        System.out.println(sumDub2);
+
+        int sumDub2Int = (int) sumDub2;
+        System.out.println(sumDub2Int);
+
+        //Question #6
+
+        int x= 5;
+        double y = 6;
+        double q = y/x;
+        System.out.println(q);
+
+
+        //Question #7
+        final int ALL_CAPS = 4;
+        final double VERY_LOUD = 6.3;
+        double sumAllCaps = ALL_CAPS + VERY_LOUD;
+        System.out.println(sumAllCaps);
+
+        //Question #8
+
+        String coffee = "coffee";
+        String tea = "tea";
+        String espresso = "espresso";
+
+        //Got stuck!
+
+
+
+
+
+
+
+    }
+}
