4d73728c091e5fae8975cfbe69f954df5f5f035e
LAPTOP-07M3D52O\codenizam <codenizam@gmail.com>
2024-05-06 08:02:09 -0400

    multiplication tables

--- java-classwork/Java-Classwork/src/main/java/May_4_Weekend_Homework/PA_303_5_1_Loops/Loops.java
+++ java-classwork/Java-Classwork/src/main/java/May_4_Weekend_Homework/PA_303_5_1_Loops/Loops.java
@@ -2,9 +2,20 @@ package May_4_Weekend_Homework.PA_303_5_1_Loops;
 
 public class Loops {
 
+
     public static void main(String[] args) {
 
+        int tableSize = 10;
 
-      //spiked in difficulty
+
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j <= tableSize; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
         }
     }
+    //spiked in difficulty
+    //used ChatGPT
+}
+
